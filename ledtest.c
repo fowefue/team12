6773a2b1e6ec258b73cdb26ec68ecc74e8f06288
Yoon,Sunju <yoonsunju0508@gmail.com>
2023-11-26 18:51:44 +0900

    led code

---
+++ ledtest.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "led.h"
+#define LED_DRIVER_NAME "/dev/periled"
+int main(){
+ledLibInit();
+sleep(3);
+ledOnOff(1,1);
+ledLibExit();
+return 0;
+}
